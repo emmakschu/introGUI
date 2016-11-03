32d3106e5f56eb6098707178ad1c79f9d5b3fbc5
Michael Schumacher <mschu528@gmail.com>
2016-11-03 09:16:06 -0500

    almost done fixing deleted firstapp.c

--- GTK/C/FirstApp/firstapp.c
+++ GTK/C/FirstApp/firstapp.c
@@ -21,3 +21,24 @@ static void first_app_activate (GApplication *app)
     win = first_app_window_new (FIRST_APP (app));
     gtk_window_present (GTK_WINDOW (win));
 }
+
+static void first_app_open (GApplication *app,
+                            GFile **files,
+                            gint n_files,
+                            const gchar *hint)
+{
+    GList *windows;
+    FirstAppWindow *win;
+    int i;
+
+    windows = gtk_application_get_windows (GTK_APPLICATION (app));
+    if (windows)
+        win = FIRST_APP_WINDOW (windows->data);
+    else
+        win = first_app_window_new (FIRST_APP (app));
+
+    for (i = 0; i < n_files; i++)
+        first_app_window_open (win, files[i]);
+
+    gtk_window_present (GTK_WINDOW (win));
+}
